e429504cc54fb3459d084ddafdb94d2826a3a2b8
22DP3KZari <131261317+22DP3KZari@users.noreply.github.com>
2024-01-30 09:26:34 +0000

    Comat

---
+++ src/main/java/rvt/SmallestVal.java
@@ -0,0 +1,14 @@
+package rvt;
+
+public class SmallestVal {
+    public static int smallest(int[] array){
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < smallest){
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+    
+}
